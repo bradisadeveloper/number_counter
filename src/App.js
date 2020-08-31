04298d53c68535072aaac50a1be182dcb3983b86
bradisadeveloper <brad.o.alder@gmail.com>
2020-08-31 19:35:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super() 
+      this.state = {
+        count: 0
+      }
+      // Had to bind both handleClick and resetToZero methods inside constructor to make the methods work
+      this.handleClick = this.handleClick.bind(this)
+      this.resetToZero = this.resetToZero.bind(this)
+    }
+    // Increment the number by one
+    handleClick() {
+      this.setState(prevState => {
+        return {
+          count: prevState.count + 1
+        }
+      })
+    }
+    // Reset the count to zero
+    resetToZero() {
+      this.setState(prevState => {
+        return {
+          count: 0
+        }
+      })
+    }
+
+    render() {
+      return(
+        <div>
+          <h1>Number Counter</h1>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.handleClick}>Change!</button>
+          <button onClick={this.resetToZero}>Reset</button>
+        </div>
+      )
+    }
+  }
 
 export default App;
